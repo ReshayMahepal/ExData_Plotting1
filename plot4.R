816c762f564a0e49f34ac283725b79220e3c4651
ReshayMahepal <58564530+ReshayMahepal@users.noreply.github.com>
2020-06-27 23:09:58 +0200

    Plot4.R Corrected
    Issue encountered with trying to push the correct code used for plot4.png

--- plot4.R
+++ plot4.R
@@ -14,8 +14,29 @@ dateTime <-as.POSIXct(strptime(paste(powSum$Date, powSum$Time, sep=" "),"%d/%m/%
 powSum <- cbind(dateTime, powSum)
 ##remove any na values
 powSum <- powSum[complete.cases(powSum), ]
-##Create Plot 2
-png(filename = "plot2.png", width = 480, height = 480)
+
+##Create Plot 4
+png(filename = "plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+
+##Plot 2 - Top Left
 with(powSum, plot(dateTime,Global_active_power, type = "l", col = "black", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+
+##Top right dateTime and Volatage
+with(powSum, plot(dateTime,Voltage, type = "l", col = "black", xlab = "datetime", ylab = "Voltage"))
+
+##bottom left Plot 3
+with(powSum, 
+     {plot(dateTime, Sub_metering_1, type="l", col = "black", xlab="", ylab="Energy sub metering")
+       lines(dateTime,Sub_metering_2, type = "l", col = "red")
+       lines(dateTime,Sub_metering_3, type = "l", col = "blue")
+       legend("topright", lty = "solid", col = c("black","red","blue"),
+              legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+     })
+
+##bottom right datetime and Global Reactive Power
+with(powSum, plot(dateTime,Global_reactive_power, type = "l", col = "black", xlab = "datetime", ylab = "Global_reactive_power"))
+
 dev.off()
 
